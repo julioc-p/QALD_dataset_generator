974bf9a2e01a80f505f27693f328e0d42ea1f37e
julioc-p <14-10820@usb.ve>
2025-01-20 16:46:47 +0100

    get qald queries from all sources and dump in a json
    Signed-off-by: julioc-p <14-10820@usb.ve>

--- main.py
+++ main.py
@@ -5,26 +5,18 @@ from helpers import (
 )
 import requests
 import pandas as pd
+import json
 
-XML_SOURCES = [
-    "https://raw.githubusercontent.com/ag-sc/QALD/refs/heads/master/3/data/dbpedia-train.xml",
-    "https://raw.githubusercontent.com/ag-sc/QALD/refs/heads/master/3/data/dbpedia-test.xml",
-    "https://raw.githubusercontent.com/ag-sc/QALD/refs/heads/master/5/data/qald-5_test.xml",
-]
-JSON_SOURCES = [
-    "https://raw.githubusercontent.com/ag-sc/QALD/refs/heads/master/6/data/qald-6-test-multilingual.json",
-    "https://raw.githubusercontent.com/ag-sc/QALD/refs/heads/master/6/data/qald-6-train-multilingual.json",
-    "https://raw.githubusercontent.com/ag-sc/QALD/refs/heads/master/5/data/qald-5_train.json",
-    "https://raw.githubusercontent.com/ag-sc/QALD/refs/heads/master/7/data/qald-7-train-multilingual.json",
-    "https://raw.githubusercontent.com/ag-sc/QALD/refs/heads/master/9/data/qald-9-train-multilingual.json",
-    "https://raw.githubusercontent.com/ag-sc/QALD/refs/heads/master/9/data/qald-9-test-multilingual.json",
-    "https://raw.githubusercontent.com/ag-sc/QALD/refs/heads/master/7/data/qald-7-test-multilingual.json",
-    "https://raw.githubusercontent.com/KGQA/QALD_9_plus/refs/heads/main/data/qald_9_plus_train_dbpedia.json",
-    "https://raw.githubusercontent.com/KGQA/QALD_9_plus/refs/heads/main/data/qald_9_plus_test_dbpedia.json",
-]
+SOURCE_FILE = "sources/qald_urls.json"
+
+with open(SOURCE_FILE, "r") as json_file:
+    sources = json.load(json_file)
+    xml_sources = sources["xml"]
+    json_sources = sources["json"]
+
 if __name__ == "__main__":
     all_data = []
-    for url in XML_SOURCES:
+    for url in xml_sources:
         response = requests.get(url)
         if response.status_code == 200:
             try:
@@ -33,8 +25,8 @@ if __name__ == "__main__":
                 print(f"Failed to parse XML file with url {url}. Error: {e}")
         else:
             print(f"Failed to fetch XML file. Status code: {response.status_code}")
-    #print(all_data[0].sparql_query[0])
-    for url in JSON_SOURCES:
+
+    for url in json_sources:
         response = requests.get(url)
         if response.status_code == 200:
             try:
@@ -45,6 +37,12 @@ if __name__ == "__main__":
             print(f"Failed to fetch JSON file. Status code: {response.status_code}")
 
     all_data_df = pd.concat(all_data)
-    
-    valid_data_df = eliminate_invalid_sparql_queries(all_data_df)
-    print(valid_data_df)
+    # elimininate duplicate rows
+    all_data_df.drop_duplicates(
+        subset=["text_query", "language", "sparql_query"], inplace=True
+    )
+    # store all data in a file
+    all_data_df.to_csv("all_data.csv", index=False)
+
+    # valid_data_df = eliminate_invalid_sparql_queries(all_data_df)
+    # print(valid_data_df)
